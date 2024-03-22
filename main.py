8350d3b8dd109cfab3b80974b013e98482bd0623
longuivic <longuivictor@gmail.com>
2024-03-21 21:30:51 -0300

    using dg_combined to create profitable customers and sales  seasonality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import datetime
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 
@@ -36,7 +37,7 @@ df_csv = df_csv[cols]
 df_csv['valor_total'] = df_csv['preco'] * df_csv['quantidade']
 
 # Printing table name before printing CSV DataFrame
-print("Tabela CSV: vendas_db")
+print("Tabela CSV: vendas_df")
 print("--------------------------------------------------------------------------------")
 print(df_csv.head())
 print("\n" * 2)  # Adding spacing between DataFrames
@@ -48,3 +49,30 @@ df_combined = pd.concat([df_sqlite, df_csv], ignore_index=True)
 print("Tabela combinada:")
 print("--------------------------------------------------------------------------------")
 print(df_combined)
+print("\n" * 2)  # Adding spacing between DataFrames
+
+# Converting the 'data_compra' column to the datetime type
+df_combined['data_compra'] = pd.to_datetime(df_combined['data_compra'])
+
+# Extracting the month from the purchase date
+df_combined['mes_compra'] = df_combined['data_compra'].dt.month
+
+# Calculating the total sales value per customer
+clientes_lucrativos = df_combined.groupby('cliente_nome')['valor_total'].sum().reset_index()
+
+# Sort customers based on total sales value (highest to lowest)
+clientes_lucrativos = clientes_lucrativos.sort_values(by='valor_total', ascending=False)
+
+# Printing the most profitable customers
+print("Clientes mais lucrativos:")
+print("------------------------------")
+print(clientes_lucrativos.head())
+print("\n" * 2)  # Adding spacing between DataFrames
+
+# Calculating total sales per month to analyze sales seasonality
+vendas_por_mes = df_combined.groupby('mes_compra')['valor_total'].sum().reset_index()
+
+# Printing sales seasonality by month
+print("Sazonalidade de vendas por mês:")
+print("----------------------------------")
+print(vendas_por_mes)
